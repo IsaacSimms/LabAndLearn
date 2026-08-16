41fe3b3db7c03bd2c12a62efa05503f0b1490a71
Isaac Simms <isaacsimms11@gmail.com>
2026-08-16 15:14:53 -0500

    another rep on project setup / building (compiling, linking, and the toolchain) initial "Hello world" style print out

--- TrainC++/FileWalker/main.cpp
+++ TrainC++/FileWalker/main.cpp
@@ -1,2 +1,13 @@
-// == FileWalker, by Isaac Simms                                 == //
-// == Recursively walk a directory and report useful information == //
+// == FileWalker, by Isaac Simms                                              == //
+// == Perform actions on files and directories                                == //
+// == inital implementation has a CLI that can be used for file concatenation == //
+
+// == include headers == //
+#include <iostream>
+#include <filesystem>
+
+// == entry point == //
+int main () {
+    std::cout <<"// == Entering FileWalker == //" << std::endl;
+    return 0;
+}
